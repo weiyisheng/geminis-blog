45ddb9d8a5020cf7c07aa4bcd7e0ca645ec677ae
weiyisheng <weiyisheng1991@icloud.com>
2017-04-01 11:11:12 +0800

    pushall

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps : [
     {
       name      : "Blog",
-      script    : "server.js",
+      script    : "./server.js",
       env: {
         COMMON_VARIABLE: "true"
       },
