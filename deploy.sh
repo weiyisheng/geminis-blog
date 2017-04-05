fec2dbcd503f5c37def697a105411cc3049c39b4
weiyisheng <weiyisheng1991@icloud.com>
2017-04-05 09:56:30 +0800

    deploy sh file change

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,11 @@
-pm2 deploy ecosystem.config.js production
+#!/bin/bash
+git add .
+if [ "$1" ]
+then
+  git commit -m "$1"
+  git pull
+  git push
+  pm2 deploy ecosystem.config.js production
+else
+  echo "Failed: give some message"
+fi
